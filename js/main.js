2c143435d4d464e618888fb6628fc59a9387cd91
Yann <yann@sugarsplashes.com>
2013-08-12 14:49:53 +0400

    mustache

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function(){
-            self.showAlert('Store initialized', 'Info');
+        this.store = new LocalStorageStore(function(){
+             $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     showAlert: function (message, title) {
