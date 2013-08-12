a49a17d6432893c0c925a1a2b3f00f072a62dfa4
Yann <yann@sugarsplashes.com>
2013-08-12 14:58:41 +0400

    iscroll

--- js/HomeView.js
+++ js/HomeView.js
@@ -17,6 +17,13 @@ var HomeView = function(store) {
     {
         store.findByName($('.search-key').val(), function(employees) {
             $('.employee-list').html(HomeView.liTemplate(employees));
+             if (self.iscroll) {
+                console.log('Refresh iScroll');
+                self.iscroll.refresh();
+            } else {
+                console.log('New iScroll');
+                self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+            }
         });
     };
  	
