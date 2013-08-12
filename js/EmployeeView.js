66d1c2599d15a044143a560b5805e414c2758db7
Yann <yann@sugarsplashes.com>
2013-08-12 16:28:51 +0400

    take pic

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,7 +2,7 @@ var EmployeeView = function(employee) {
 
  	this.initialize = function() {
         this.el = $('<div/>');
-        this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.change-pic-btn', this.changePicture);
     };
  
     this.initialize();
@@ -11,6 +11,31 @@ var EmployeeView = function(employee) {
 	    this.el.html(EmployeeView.template(employee));
 	    return this;
 	};
+
+	this.changePicture = function(event) {
+	    event.preventDefault();
+	    if (!navigator.camera) {
+	        app.showAlert("Camera API not supported", "Error");
+	        return;
+	    }
+	    var options =   {   quality: 50,
+	                        destinationType: Camera.DestinationType.DATA_URL,
+	                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+	                        encodingType: 0     // 0=JPG 1=PNG
+	                    };
+	 
+	    navigator.camera.getPicture(
+	        function(imageData) {
+	            $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+	        },
+	        function() {
+	            app.showAlert('Error taking picture', 'Error');
+	        },
+	        options);
+	 
+	    return false;
+	};
+
  
 }
 
