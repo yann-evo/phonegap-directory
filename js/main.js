5de9826b0774546e2799d865527694e8a5e28e49
Yann <yann@sugarsplashes.com>
2013-08-12 14:13:38 +0400

    alert

--- js/main.js
+++ js/main.js
@@ -14,9 +14,20 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function(){
+            self.showAlert('Store initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
 };
 
